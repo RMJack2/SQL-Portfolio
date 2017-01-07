3aefc1c65cb1d3bdcb4a50c761717cc7dd89e061
RMJack2 <rjack10229@gmail.com>
2017-01-07 17:15:18 -0500

    Delete Alias Within Select Statement.sql

--- Alias Within Select Statement.sql
+++
@@ -1,9 +0,0 @@
---This code demonstrates how to use aliases inside select statements to
---rename columns
-USE VWRProduction;
-GO
-
-SELECT OrderID, CustomerID AS 'Customer Identification' , AssociateID, OrderDate AS 'Product Orderdate', 
-       ShippedDate AS 'Date Shipped'
-FROM Sales.CustomerOrders
-ORDER BY OrderID;
